4fc0821af4f14dd9b4dcd355d627e4f038176560
razoxik <BartosekTom@gmail.com>
2017-10-17 20:19:41 +0200

    Test

--- src/main/java/com/astora/web/service/LoginService.java
+++ src/main/java/com/astora/web/service/LoginService.java
@@ -1,17 +1,9 @@
 package com.astora.web.service;
 
-import com.astora.web.dto.model.UserModel;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-import java.util.Collection;
-
 /**
  * @author <a href="mailto:maresjan694@gmail.com">Jan Mares</a>, 16.10.2017
  */
-public class LoginService implements UserDetailsService {
+/*public class LoginService implements UserDetailsService {
 
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserModel user = new UserModel();
@@ -19,4 +11,4 @@ public class LoginService implements UserDetailsService {
         user.setUsername("admin");
         return user;
     }
-}
+}*/
