769892b5bfb5074ccb688396a0442baf8bbcbe78
razoxik <BartosekTom@gmail.com>
2017-10-18 00:15:02 +0200

    Presunuti slozky "web" pod slozku "main" (a zaroven prejmenovani na "webapp") ready to go, uz to konecne jede jak ma :)

--- src/main/java/com/astora/web/service/LoginService.java
+++ src/main/java/com/astora/web/service/LoginService.java
@@ -1,14 +1,19 @@
 package com.astora.web.service;
 
+import com.astora.web.dto.model.UserModel;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 /**
  * @author <a href="mailto:maresjan694@gmail.com">Jan Mares</a>, 16.10.2017
  */
-/*public class LoginService implements UserDetailsService {
+ public class LoginService implements UserDetailsService {
 
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserModel user = new UserModel();
+        UserModel user = new  UserModel();
         user.setPassword("123");
         user.setUsername("admin");
         return user;
     }
-}*/
+}
